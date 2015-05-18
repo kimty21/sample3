ea29906586e7826fd39e639fe81dc81ce5c03b19
kimty21 <kimty2112@gmail.com>
2015-05-18 14:22:27 +0900

    5번

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello 정한수 대리님");
+        System.out.println("Hello ktds");
         
         }
 
