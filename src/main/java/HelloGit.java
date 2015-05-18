c9b74fb5de249a2a95102805d2359617f8355e4a
kimty21 <kimty2112@gmail.com>
2015-05-18 11:24:39 +0900

    Development branch에서  HelloHit.java를 수정함

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,9 +2,11 @@
  * Created by Taeyeon on 2015. 5. 18..
  */
 public class HelloGit {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello Git");
 
-
+    }
+    public static int add(int a , int b){
+        return a + b;
     }
 }
