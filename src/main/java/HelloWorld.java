50f6d5f48148c3e301745980b83ea0d25d706567
kimty21 <kimty2112@gmail.com>
2015-05-18 11:05:00 +0900

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Taeyeon on 2015. 5. 18..
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        
+        }
+
+
+}
